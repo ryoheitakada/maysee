ba53959b710a361be638269afbff6e9a4f4ddc12
ryohei <nikusuki727@gmail.com>
2014-04-05 16:38:50 +0900

    deploy/production.rb変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,11 +9,11 @@ set :rails_env, 'production'
 set :deploy_to, '/var/www/maysee/'
 
 # Your HTTP server, Apache/etc
-role :web, "54.238.59.147"
+role :web, "ec2-54-238-59-147.ap-northeast-1.compute.amazonaws.com"
 # This may be the same as your `Web` server
-role :app, "54.238.59.147"
+role :app, "ec2-54-238-59-147.ap-northeast-1.compute.amazonaws.com"
 # This is where Rails migrations will run
 # rake db:migrateを実行するサーバーのホスト名を指定する
 # データベースサーバーのホスト名になるわけではないので注意
-role :db,  "54.238.59.147", primary: true
+role :db,  "ec2-54-238-59-147.ap-northeast-1.compute.amazonaws.com", primary: true
 
